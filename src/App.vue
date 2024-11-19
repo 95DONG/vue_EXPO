2f2a0ba351808f9a05fb137ad95e68bed618047e
胥冬冬 <xudd@g-soft.com.cn>
2024-11-19 15:45:00 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import home from './views/HomeView.vue'
+export default {
+  components: {
+    home
+  }
+}
+</script>
+
+<style>
+</style>
+
+
+<style >
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
